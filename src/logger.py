9f1c21de8023962b263b2327bffd1ca25335ac2a
Divyam-Dave12 <divyamdave2003@gmail.com>
2025-07-15 23:14:55 +0530

    Data and EDA and

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,19 @@ import logging
 import os
 from datetime import datetime
 
+# Generate log file name
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Create 'logs' directory if it doesn't exist
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Full path to log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Set up basic logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO
-
+    level=logging.INFO
 )
